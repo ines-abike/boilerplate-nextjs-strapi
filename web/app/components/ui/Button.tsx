a1e8d697104b4ae1edf44d7a263f04c117abcd56
GitOmonwale <inesagbozo@gmail.com>
2026-03-19 18:57:29 +0100

    feat: add ui and layout components

---
+++ web/app/components/ui/Button.tsx
@@ -0,0 +1,20 @@
+// components/Button.tsx
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, className = "", ...props }) => {
+  return (
+    <button
+      className={`bg-primary-500 cursor-pointer hover:bg-primary-600 text-white text-base px-6 py-3 rounded-md transition-colors ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
